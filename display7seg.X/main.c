ef90aaf0c309bf1ae81b54ff48446b52ac86f42d
20185090 <20185090@DSN1271158634.cfp127aluno.local>
2021-02-24 16:36:34 -0300

    Teste Inicial

--- display7seg.X/main.c
+++ display7seg.X/main.c
@@ -11,25 +11,17 @@
 #include "delay.h"
 #include "display7seg.h"
 
-char  vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
-                    0x66, 0x6D, 0x7D, 0x07,
-                    0x7F, 0x6F, 0x77, 0x7C,
-                    0x39, 0x5E, 0x79, 0x71};
-    
-
-    
 
 void main(void) 
 {
     int cont = 0;
     display7seg_init();
-    
-    while (1)
+    while( 1 )
     {
-       display7seg( vetor[cont] );
-       delay (1000);
-       cont++;
-       if( cont >= 16)
-           cont = 0;
+        display7seg( cont );
+        delay( 1000 );
+        cont++;
+        if( cont >= 16 )
+            cont = 0;
     }
 }
